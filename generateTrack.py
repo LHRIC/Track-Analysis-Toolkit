2252f8160a01f5ce590bd20044cf4e6c57fea731
Tacocat0254 <tacocat0254@gmail.com>
2024-09-28 15:53:48 -0500

    finished issue 1, created generateTrack class

--- generateTrack.py
+++ generateTrack.py
@@ -1,3 +1,23 @@
 # have a function to take in lat and long to generate a track
 # have a function to take in wheel speed data to generate a velocity trait track
 # have a function to take in IMU data to generate a GGV
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class GenerateTrack:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.df = None
+    
+    def load_data(self):
+        self.df = pd.read_csv(self.filepath)
+    
+    def plot_coordinates(self):
+        if self.df is not None:
+            plt.figure(figsize=(10, 10))
+            plt.scatter(x=self.df['Longitude'], y=self.df['Latitude'])
+            plt.xlabel('Longitude')
+            plt.ylabel('Latitude')
+            plt.title('Track Map')
+            plt.show()
--- parsecsv.py
+++ parsecsv.py
@@ -1,1 +1,10 @@
 # Implement a function to take in the driveday.csv and be able to get the corrdinates needed
+from generateTrack import GenerateTrack
+
+def main(): 
+    plotter = GenerateTrack("driveday.csv")
+    GenerateTrack.load_data(plotter)
+    GenerateTrack.plot_coordinates(plotter)
+
+if __name__ == "__main__":
+    main()
